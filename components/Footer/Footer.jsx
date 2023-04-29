f8332dc8f1e99811aa2e5b41a268e6fbd34460d4
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-04-29 10:20:54 +0600

    add auto year

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -14,16 +14,13 @@ const Footer = () => {
         <Row>
           <Col lg="12" className={`${classes.footer__top}`}>
             <div className={`${classes.footer__creator}`}>
-              <h6>Built with ❤️ by Taraqul Islam Rony</h6>
+              <h6>Built by Shakil Ahmed</h6>
             </div>
           </Col>
           {/* ========= footer bottom ========= */}
           <Col lg="12">
             <div className={`${classes.footer__copyright}`}>
-              <h6>
-                &copy; {year} - Taraqul Islam Rony.  All right
-                reserved.{" "}
-              </h6>
+              <h6>&copy; {year} - Shakil Ahmed. All right reserved. </h6>
             </div>
           </Col>
         </Row>
