5f041a9610c9904e315e164994ff345580c088e3
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-04-29 10:23:54 +0600

    add section

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Fragment } from "react";
 import Hero from "../components/UI/Hero";
 import Work from "../components/UI/Work";
 import About from "../components/UI/About";
-import Portfolio from "../components/UI/Portfolio";
+import Projects from "../components/UI/Projects";
 import Tools from "../components/UI/Tools";
 import Contact from "../components/UI/Contact";
 
@@ -11,9 +11,9 @@ export default function Home() {
   return (
     <div>
       <Head>
-        {/* <title>Taraqul Islam Rony</title>
+        {/* <title>Shakil Ahmed</title>
         <meta data-react-helmet="true" name="description" content="Online software development portfolio." />
-        <meta data-react-helmet="true" property="og:title" content="Taraqul Islam Rony | Full Stack Software Engineer"></meta>
+        <meta data-react-helmet="true" property="og:title" content="Shakil Ahmed| Full Stack Software Engineer"></meta>
         <meta data-react-helmet="true" property="og:description" content="Online software development portfolio."></meta>
         <meta data-react-helmet="true" property="og:type" content="website"></meta>
         <meta property="og:url" content="YOUR_URL"></meta>
@@ -24,30 +24,51 @@ export default function Home() {
         <meta data-react-helmet="true" name="twitter:description" content="Online software development portfolio."></meta> */}
 
         {/* <!-- Primary Meta Tags --> */}
-        <title>Taraqul Islam Rony 👨‍💻 | Portfolio 📃</title>
-        <meta name="title" content="Taraqul Islam Rony 👨‍💻 | Portfolio 📃" />
-        <meta name="description" content="Online software development portfolio." />
+        <title>Shakil Ahmed</title>
+        <meta name="title" content="Shakil Ahmed | Portfolio 📃" />
+        <meta
+          name="description"
+          content="Online software development portfolio."
+        />
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://tirony.netlify.app/" />
-        <meta property="og:title" content="Taraqul Islam Rony 👨‍💻 | Portfolio 📃" />
-        <meta property="og:description" content="Online software development portfolio." />
-        <meta property="og:image" content="https://res.cloudinary.com/hawktech-cloud/image/upload/v1669712926/Portfolio_nilyjm.png" />
+        <meta
+          property="og:title"
+          content="Taraqul Islam Rony 👨‍💻 | Portfolio 📃"
+        />
+        <meta
+          property="og:description"
+          content="Online software development portfolio."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/hawktech-cloud/image/upload/v1669712926/Portfolio_nilyjm.png"
+        />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta data-react-helmet="true" name="twitter:creator" content="@dariansampare"></meta>
+        <meta
+          data-react-helmet="true"
+          name="twitter:creator"
+          content="@dariansampare"
+        ></meta>
         <meta property="twitter:url" content="https://metatags.io/" />
-        <meta property="twitter:title" content="Taraqul Islam Rony 👨‍💻 | Portfolio 📃" />
-        <meta property="twitter:description" content="Online software development portfolio." />
-        <meta property="twitter:image" content="https://res.cloudinary.com/hawktech-cloud/image/upload/v1669712926/Portfolio_nilyjm.png" />
-
+        <meta property="twitter:title" content="Shakil Ahmed | Portfolio 📃" />
+        <meta
+          property="twitter:description"
+          content="Online software development portfolio."
+        />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/hawktech-cloud/image/upload/v1669712926/Portfolio_nilyjm.png"
+        />
       </Head>
       <Fragment>
         <Hero />
         <Work />
-        <Portfolio />
+        <Projects />
         <About />
         <Tools />
         <Contact />
