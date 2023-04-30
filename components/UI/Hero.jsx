89ee0da9050c0aabe3742455c29e9bede78e517e
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-04-30 18:18:35 +0600

    update new

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -3,7 +3,7 @@ import SectionSubtitle from "./SectionSubtitle";
 import { Container, Row, Col } from "reactstrap";
 import Image from "next/image";
 import Link from "next/link";
-import heroImg from "../../public/images/hero.jpg";
+import heroImg from "../../public/images/hero.png";
 import classes from "../../styles/hero.module.css";
 
 const Hero = () => {
@@ -17,10 +17,22 @@ const Hero = () => {
               {/* <SectionSubtitle subtitle="Hello" /> */}
               <h3 className="mt-3 mb-3">Hi, 👋</h3>
               <h2 className="mt-3 mb-3">I&apos;m Shakil Ahmed</h2>
-              <h5 className="mb-4">
+              <h5 className="mb-4 color:red text-justify">
                 {/* I&apos;m a Full Stack Software Engineer & Die Hard Gamer. */}
-                Full Stack Developer | Building WEB 3.0 App | Ethereum | NodeJS
-                | Smart Contract Auditor
+                {/* Full Stack Developer | Building WEB 3.0 App | Ethereum | NodeJS
+                | Smart Contract Auditor I am a full Stack Software Engineer
+                based in Dhaka, Bangladesh. Working towards creating software
+                that makes life easier and more meaningful.<br></br> */}
+                With a passion for both Blockchain and full Stack software
+                engineering, I am committed to helping organizations leverage
+                the power of decentralized technology. Whether its designing
+                smart contracts, building decentralized applications.
+                <br />
+                <br />
+                In addition to my technical skills, I am a quick learner, a
+                problem solver, and a team player. I am eager to collaborate
+                with other professionals in the blockchain industry to apply my
+                knowledge and skills in real-world projects.
               </h5>
               <p></p>
               <div className="mt-5">
@@ -37,14 +49,14 @@ const Hero = () => {
                   {"Resume 📃"}
                 </Link> */}
                 <a
-                  // className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
                   style={{
                     textDecoration: "none",
                     color: "#01d293",
                     fontSize: "25px",
                     fontWeight: "500",
                   }}
-                  href="./file/CV_of_Shakil_Ahmed.pdf"
+                  href="./files/CV_of_Shakil_Ahmed.pdf"
+                  // href="./file/CV_of_Shakil_Ahmed.pdf"
                   download
                 >
                   Resume 📃
@@ -56,7 +68,7 @@ const Hero = () => {
           {/* ========== hero img ============= */}
           <Col lg="6" md="6">
             <div className={`${classes.hero__img} text-end`}>
-              <Image alt="hero-image" src={heroImg} width="400" height="400" />
+              <Image alt="hero-image" src={heroImg} width="400" height="500" />
             </div>
           </Col>
         </Row>
