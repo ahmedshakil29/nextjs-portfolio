9d1d5ba35f2ee3dc2fd824efbeec097fd21b7b8a
shakil ahmed <ahmedshakil.aiub@gmail.com>
2023-04-30 18:18:24 +0600

    add project

--- components/data/portfolio.js
+++ components/data/portfolio.js
@@ -2,16 +2,26 @@
 // https://github.com/TIRony/my_portfolio
 
 const portfolio = [
-  {
-    id: "01",
-    title: "E-Commerce store",
-    img: "/images/portfolio 01.jpg",
-    category: "Projects",
-    keyword: ["REACT", "NODE", "EXPRESS", "MONGODB", "ANT DESIGN", "REDUX", "HEROKU", "NETLIFY"],
-    liveUrl: "https://hawk-tech.netlify.app/",
-    gitUrl: "null",
-    description: "It's a Full-stack e-Commerce web application. The goal of this project is to build an eCommerce website that is fast, easy to update, and mobile responsive. And I'm trying to make it a single-page application for better use.",
-  },
+  // {
+  //   id: "01",
+  //   title: "E-Commerce store",
+  //   img: "/images/portfolio 01.jpg",
+  //   category: "Projects",
+  //   keyword: [
+  //     "REACT",
+  //     "NODE",
+  //     "EXPRESS",
+  //     "MONGODB",
+  //     "ANT DESIGN",
+  //     "REDUX",
+  //     "HEROKU",
+  //     "NETLIFY",
+  //   ],
+  //   liveUrl: "https://hawk-tech.netlify.app/",
+  //   gitUrl: "null",
+  //   description:
+  //     "It's a Full-stack e-Commerce web application. The goal of this project is to build an eCommerce website that is fast, easy to update, and mobile responsive. And I'm trying to make it a single-page application for better use.",
+  // },
 
   {
     id: "02",
@@ -21,7 +31,8 @@ const portfolio = [
     keyword: ["NEXT", "NODE", "EXPRESS", "MYSQL", "BOOTSTRAP"],
     liveUrl: "/",
     gitUrl: "null",
-    description: "It is a Single Page Application (SPA). I make it to present myself to others. It's fast, easy to update and mobile responsive.",
+    description:
+      "It is a Single Page Application (SPA). I make it to present myself to others. It's fast, easy to update and mobile responsive.",
   },
 
   {
@@ -30,31 +41,55 @@ const portfolio = [
     img: "/images/portfolio 03.jpg",
     category: "Projects",
     keyword: ["PHP", "EXPRESS", "NODE", "JAVASCRIPT", "CSS", "HTML5", "AJAX"],
-    liveUrl: "https://drive.google.com/file/d/1Tkg-hKavyQK24PXmZvNd2Bz34MQC2G3Y/view?usp=share_link",
+    liveUrl:
+      "https://drive.google.com/file/d/19XiMIk2ROZIZL3AEimSvYByJKCga-GXo/view?usp=sharing",
     gitUrl: "https://github.com/TIRony/Online_Worker_service_provider_NodeJs",
-    description: "This project is about providing online services as needed like a plumber or electrician etc. This project was in our curriculum, and after graduation, I added a pinch of salt to make it more palatable. ",
+    description:
+      "This project is about providing online services as needed like a plumber or electrician etc. This project was in our curriculum, and after graduation, I added a pinch of salt to make it more palatable. ",
   },
-
   {
     id: "04",
+    title: "Inventory Management System",
+    img: "/images/IMS.png",
+    category: "Projects",
+    keyword: [
+      "ASP.NET Core",
+      "EF Core",
+      "SQL Server",
+      "C#",
+      "CSS",
+      "HTML5",
+      "Blazor",
+    ],
+    liveUrl: "null",
+    gitUrl:
+      "https://github.com/ahmedshakil29/.NET-Core-Inventory_Management_System",
+    description:
+      "ASP.NET Core Blazor in .NET 6 using Entity Framework Core, SQL Server, Clean Architecture and ASP.NET Core Identity for Authentication and Authorization. ",
+  },
+  {
+    id: "05",
     title: "Service worker provider",
     img: "/images/portfolio 03.jpg",
     category: "Academic Projects",
     keyword: ["PHP", "JAVASCRIPT", "CSS", "HTML5", "AJAX"],
-    liveUrl: "https://drive.google.com/file/d/1Tkg-hKavyQK24PXmZvNd2Bz34MQC2G3Y/view?usp=share_link",
-    gitUrl: "https://github.com/TIRony/Online-Worker-Service-Provider",
-    description: "This project is about providing online services as needed like a plumber or electrician etc. which is in our course curriculum under MD. Al-Amin, Lecturer, Computer Science, American International University-Bangladesh (AIUB).",
+    liveUrl:
+      "https://drive.google.com/file/d/19XiMIk2ROZIZL3AEimSvYByJKCga-GXo/view?usp=sharing",
+    gitUrl: "https://github.com/ahmedshakil29/WebTech-Easy4U",
+    description:
+      "This project is about providing online services as needed like a plumber or electrician etc. which is in our course curriculum under MD. Al-Amin, Lecturer, Computer Science, American International University-Bangladesh (AIUB).",
   },
-  
+
   {
-    id: "05",
-    title: "Hospital Management system",
-    img: "/images/portfolio 05.jpg",
+    id: "06",
+    title: "House Management system",
+    img: "/images/HMS.PNG",
     category: "Academic Projects",
-    keyword: ["C#", "ENTITY", "METRO MODERN UI", "MYSQL"],
+    keyword: ["C#", "ENTITY", "MYSQL"],
     liveUrl: "null",
-    gitUrl: "https://github.com/TIRony/Hospital-Management-System",
-    description: "This project is about Hospital managemant system. which is in our course curriculum under Jahid Hassan, Senior Programmer, Software Development Department, American International University-Bangladesh (AIUB).",
+    gitUrl: "https://github.com/ahmedshakil29/HouseManagement",
+    description:
+      "The House Management System is Searching in Based on the Apartment, Office, House in Dhaka city.This system is based on the Owners and the Customers.",
   },
 ];
 
