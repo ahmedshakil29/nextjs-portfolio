d32c6593b3659113f98cdead6c3c7146e02bd915
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2025-02-21 03:35:01 +0600

    update about

--- components/UI/About.jsx
+++ components/UI/About.jsx
@@ -15,8 +15,8 @@ import EndDiv from "./EndDiv";
 import Qualification from "./Qualification";
 
 const About = () => {
-  const myself = "// About myself";
-  const moreDetails = "// Keep reading for more details... ⬇️";
+  const myself = "About myself";
+  const moreDetails = "Keep reading for more details... ⬇️";
   const Seeitinaction = "// See it in action";
   return (
     <section id="about">
@@ -31,7 +31,7 @@ const About = () => {
           {/* <h3 className="mt-4">I&apos;m here</h3>
           <h3 className="mb-4">to help your next project</h3> */}
           <h5 className={`${classes.about__self}`}>
-            Recent computer science and engineering graduate from American
+            Computer science and engineering graduate from American
             International University Bangladesh (AIUB) with good academic
             standing and strong professional experience. I have developed my
             abilities in software testing and web development throughout the
@@ -46,19 +46,19 @@ const About = () => {
             institute="American International University - Bangladesh (AIUB)"
             degree="Bachelor of Science in Computer Science & Engineering (B.Sc. CSE)"
             year="2017 - 2021"
-            des=" CGPA 3.71 out of 4.00 and "
+            des=" CGPA: 3.71 out of 4.00 "
           />
           <Qualification
             institute="Govt. Azizul Haque College Bogura"
             degree="Higher Secondary Certificate (HSC)"
             year="2013 - 2015"
-            des="GPA: 4.00 out of 5.00"
+            //des="GPA: 4.00 out of 5.00"
           />
           <Qualification
             institute="Sariakandi Govt. High School"
             degree="Secondary School Certificate (SSC)"
             year="2008-2013"
-            des="GPA: 5.00 out of 5.00"
+            // des="GPA: 5.00 out of 5.00"
           />
           {/* <div className="mt-5">
             <button className="primary__btn">
